fb165192870828a1453f2e6cb68c42d6ed875996
joprithiviraj <prithiviraj1522@gmail.com>
2020-06-03 12:13:31 +0530

    Initial Project Setup

---
+++ JPAlertViewController/JPAlertViewController.h
@@ -0,0 +1,19 @@
+//
+//  JPAlertViewController.h
+//  JPAlertViewController
+//
+//  Created by Prithiviraj Tamilarasan on 03/06/20.
+//  Copyright © 2020 Prithiviraj Tamilarasan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JPAlertViewController.
+FOUNDATION_EXPORT double JPAlertViewControllerVersionNumber;
+
+//! Project version string for JPAlertViewController.
+FOUNDATION_EXPORT const unsigned char JPAlertViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JPAlertViewController/PublicHeader.h>
+
+
