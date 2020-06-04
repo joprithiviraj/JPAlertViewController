9ef0d449fba8da1130686c72fb29ea1e0d303cad
joprithiviraj <prithiviraj1522@gmail.com>
2020-06-04 14:46:50 +0530

    added new file

---
+++ JPAlertViewController/JPAlertViewController.swift
@@ -0,0 +1,21 @@
+//
+//  JPAlertViewController.swift
+//  JPAlertViewController
+//
+//  Created by Prithiviraj Tamilarasan on 04/06/20.
+//  Copyright © 2020 Prithiviraj Tamilarasan. All rights reserved.
+//
+
+public final class JPAlertViewController {
+
+    let name = "JPAlertViewController"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- JPAlertViewControllerTests/JPAlertViewControllerTests.swift
+++ JPAlertViewControllerTests/JPAlertViewControllerTests.swift
@@ -11,14 +11,24 @@ import XCTest
 
 class JPAlertViewControllerTests: XCTestCase {
 
+    var jpAlertViewController: JPAlertViewController!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        jpAlertViewController = JPAlertViewController()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testAdd() {
+        XCTAssertEqual(jpAlertViewController.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(jpAlertViewController.sub(a: 2, b: 1), 1)
+    }
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
